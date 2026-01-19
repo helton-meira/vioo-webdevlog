3a05fc5514461f960ed0fd7fdaa5c96c57ad0e67
helton-meira <plismail@protonmail.com>
2026-01-19 13:41:34 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,28 @@
+import type { Config } from "tailwindcss";
 
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#1337ec",
+        "background-light": "#f6f6f8",
+        "background-dark": "#101322",
+        "tag-backend": "#7c3aed",
+        "tag-design": "#db2777",
+        "tag-ia": "#0284c7",
+        "tag-infra": "#64748b", 
+      },
+      fontFamily: {
+        "display": ["var(--font-inter)"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
